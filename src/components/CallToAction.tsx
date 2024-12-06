7333e7b99febe183823bc4d3300290b150704ee6
Nirmit Saini <nirmitsaini24@gmail.com>
2024-12-06 22:32:05 +0530

    fix

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -35,11 +35,10 @@ export const CallToAction = () => {
           />
         </motion.div>
         <h2 className="text-center text-5xl sm:text-6xl sm:max-w-[648px] mx-auto  font-bold tracking-tighter ">
-          Get Instant Access
+          Join the Waitlist{" "}
         </h2>
         <p className="text-xl text-center text-white/70 mt-5 ">
-          Celebrate the joy of accomplishment with an app designed to track your
-          progress and motivate your efforts.
+          Connect with us to elevate your brand's journey and growth together.
         </p>
         <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row ">
           <input
@@ -48,7 +47,7 @@ export const CallToAction = () => {
             className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3CF] sm:flex-1"
           />
           <button className="bg-white py-2 h-12 rounded-lg text-black px-5">
-            Get Access{" "}
+            Join Waitlist{" "}
           </button>
         </form>
       </div>
