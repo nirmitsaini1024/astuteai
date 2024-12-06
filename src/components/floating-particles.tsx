1d9d6259e91fadee6afa51fe7f11cf3f15411094
Nirmit Saini <nirmitsaini24@gmail.com>
2024-12-06 21:37:02 +0530

    fix

--- src/components/floating-particles.tsx
+++ src/components/floating-particles.tsx
@@ -21,7 +21,7 @@ export function FloatingParticles() {
 
   return (
     <>
-      {[...Array(50)].map((_, i) => (
+      {[...Array(25)].map((_, i) => (
         <motion.div
           key={i}
           className="absolute w-1 h-1 bg-violet-400/20 rounded-full hidden sm:block"
