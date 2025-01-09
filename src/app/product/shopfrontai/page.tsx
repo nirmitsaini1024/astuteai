11f5b91c721d12e5db83073dcf475b2421d10428
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-01-09 16:51:05 +0530

    fix

--- src/app/product/shopfrontai/page.tsx
+++ src/app/product/shopfrontai/page.tsx
@@ -1,14 +1,12 @@
-import { HeroContent } from "@/components/sections/Hero";
 import ProductAddresses from "@/components/sections/ProductAddresses";
 import Benefits from "@/components/sections/Benefits";
 import Resources from "@/components/sections/Resources";
-import { HeroBackground } from "@/components/sections/hero-background";
-import Hello  from "@/components/sections/Hello";
+import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white -mt-[7rem]">
-      <Hello />
+      <Hero />
       <ProductAddresses />
       <Benefits />
       <Resources />
