b5ca7b8b79553c5fae1d284d88fa882c7e4cb8bc
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-01-09 14:03:43 +0530

    fix

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -5,26 +5,21 @@ const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   try {
-    console.log("1")
     // Parse the request body
     const { name, email } = await req.json();
-    console.log("2")
 
     // Validate inputs
     if (!name || !email) {
       return NextResponse.json({ error: "Name and email are required" }, { status: 400 });
     }
-    console.log("3")
 
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
-    console.log("4")
 
     if (existingUser) {
       return NextResponse.json({ error: "Email is already in the waitlist" }, { status: 400 });
     }
-    console.log("5")
 
     // Add the user to the database
     await prisma.user.create({
@@ -34,7 +29,6 @@ export async function POST(req: NextRequest) {
         status: "Active", // Ensure this matches the enum value defined in the Prisma schema
       },
     });
-    console.log("6")
 
     return NextResponse.json({ message: "Successfully added to the waitlist!" }, { status: 200 });
   } catch (error) {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/api(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/api/users"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
