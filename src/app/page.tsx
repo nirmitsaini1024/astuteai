d368702add6676ca6051b2e72567f312b915b969
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-01-14 17:00:14 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,20 @@ import { Discover } from "@/components/Discover";
 import { ServicesSection } from "@/components/service-section";
 import FAQ from "@/components/FAQ/index";
 import Works from "./works/page";
-import LocomotiveScrollWrapper from "@/components/LocomotiveScrollWrapper ";
+// import LocomotiveScrollWrapper from "@/components/LocomotiveScrollWrapper ";
 
 export default function Home() {
   return (
-    <LocomotiveScrollWrapper>
-      <Hero />
-      <ServicesSection />
-      <Works />
-      <Discover />
-      <FAQ />
-      <CallToAction />
-    </LocomotiveScrollWrapper>
+    <>
+
+      {/* <LocomotiveScrollWrapper> */}
+        <Hero />
+        <ServicesSection />
+        <Works />
+        <Discover />
+        <FAQ />
+        <CallToAction />
+      {/* </LocomotiveScrollWrapper> */}
+    </>
   );
 }
