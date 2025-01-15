8da7e75c205f295074d1f815696e8931329e80f4
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-01-16 00:23:24 +0530

    contactus

--- src/components/Navbarr.tsx
+++ src/components/Navbarr.tsx
@@ -107,25 +107,25 @@ export default function SiteHeader() {
                             <div className="  text-sm grid grid-cols-2 gap-10 p-4">
                               <ProductItem
                                 title="ShopFrontAI"
-                                href="https://algochurn.com"
+                                href="#"
                                 src="https://assets.aceternity.com/demos/algochurn.webp"
                                 description="Prepare for tech interviews like never before."
                               />
                               <ProductItem
                                 title="Blogger AI"
-                                href="https://tailwindmasterkit.com"
+                                href="#"
                                 src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
                                 description="Production ready Tailwind css components for your next project"
                               />
                               <ProductItem
                                 title="Social Spark"
-                                href="https://gomoonbeam.com"
+                                href="#"
                                 src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
                                 description="Never write from scratch again. Go from idea to blog in minutes."
                               />
                               <ProductItem
                                 title="InsightQ"
-                                href="https://userogue.com"
+                                href="#"
                                 src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
                                 description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
                               />
