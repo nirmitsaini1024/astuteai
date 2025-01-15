ceee1f30eb328ebb885c7f3a34e7c60e8a0c9775
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-01-15 22:28:05 +0530

    fix

--- src/components/Navbarr.tsx
+++ src/components/Navbarr.tsx
@@ -97,41 +97,7 @@ export default function SiteHeader() {
                         active={active}
                         item={item}
                       >
-                        {item === "Product" && (
-                          <motion.div
-                            className="flex flex-col space-y-4 text-sm"
-                            initial={{ opacity: 0, y: 10 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ delay: 0.2, duration: 0.2 }}
-                          >
-                            <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-                              <ProductItem
-                                title="Algochurn"
-                                href="https://algochurn.com"
-                                src="https://assets.aceternity.com/demos/algochurn.webp"
-                                description="Prepare for tech interviews like never before."
-                              />
-                              <ProductItem
-                                title="Tailwind Master Kit"
-                                href="https://tailwindmasterkit.com"
-                                src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
-                                description="Production ready Tailwind css components for your next project"
-                              />
-                              <ProductItem
-                                title="Moonbeam"
-                                href="https://gomoonbeam.com"
-                                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-                                description="Never write from scratch again. Go from idea to blog in minutes."
-                              />
-                              <ProductItem
-                                title="Rogue"
-                                href="https://userogue.com"
-                                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
-                                description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-                              />
-                            </div>
-                          </motion.div>
-                        )}
+                        
                         {item === "Resources" && (
                           <motion.div
                             className="flex flex-col space-y-4 text-sm"
